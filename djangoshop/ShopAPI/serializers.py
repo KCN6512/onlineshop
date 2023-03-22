e0143fe356a5b1f82a0261861f5a42e7a3b3c0a3
Alexander <troshiy2011@mail.ru>
2023-03-22 13:37:06 +0300

    fixed import

--- djangoshop/ShopAPI/serializers.py
+++ djangoshop/ShopAPI/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from ..shopapp.models import *
+from shopapp.models import *
 
 
 class CategoriesSerializer(serializers.ModelSerializer):
