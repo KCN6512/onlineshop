91995a3a876f6db6f5da60b7c60d63b1671a7a7a
Alexander <troshiy2011@mail.ru>
2023-03-29 15:08:16 +0300

    moved email settings to environment

--- djangoshop/ShopAPI/views.py
+++ djangoshop/ShopAPI/views.py
@@ -7,7 +7,6 @@ from rest_framework.mixins import *
 from rest_framework.permissions import *
 from rest_framework.reverse import reverse
 from shopapp.models import CartModel, OrderModel, Products
-
 from .permissions import IsAdminUserOrReadOnly, IsOwnerOrReadOnly
 from .serializers import CartSerializer, OrderSerializer, ProductsSerializer
 
--- djangoshop/djangoshop/settings.py
+++ djangoshop/djangoshop/settings.py
@@ -188,10 +188,10 @@ REST_FRAMEWORK = {
     ]
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = False
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'flekso.channel@gmail.com'
-EMAIL_HOST_PASSWORD = 'ppqlgcwrkzqeedmf'
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
