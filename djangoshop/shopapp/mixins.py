e346ffbd2e91a99a1e5898a4591f92095ff79eb1
Alexander <troshiy2011@mail.ru>
2023-03-29 15:08:25 +0300

    minor comments

--- djangoshop/shopapp/mixins.py
+++ djangoshop/shopapp/mixins.py
@@ -6,6 +6,6 @@ class PriceSummaryMixin:
         price = self.products.all().aggregate(models.Sum('price')).get('price__sum')
         return price
 
-    # def price_summary_wo_aggregate(self): #  все равно 14 запросов
+    # def price_summary_wo_aggregate(self): #  все равно 14 запросов но пересчет на сервере а не базе
     #     price = sum(i.price for i in self.products.all())
     #     return price
